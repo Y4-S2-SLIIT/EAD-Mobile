6030fbc252b1cf274f04df48325e2c92846f2873
SuDelk <sudul.fernando@gmail.com>
2024-10-03 01:09:16 +0530

    products viewing

---
+++ app/src/main/java/com/app/product/Product.java
@@ -0,0 +1,57 @@
+package com.app.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String brand;
+    private String description;
+    private String category;
+    private double price;
+    private String imageUrl;
+    private int stock;
+
+    // Constructor
+    public Product(String id, String name, String brand, String description, String category, double price, String imageUrl, int stock) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.stock = stock;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
---
+++ app/src/main/java/com/app/product/ProductAdapter.java
@@ -0,0 +1,2 @@
+package com.app.product;public class ProductAdapter {
+}
